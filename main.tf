0d89af825d6a2b439f114669a27630469d8f79f3
ecsc00 <131476452+ecsc00@users.noreply.github.com>
2023-05-23 09:54:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,39 +66,51 @@ resource "aws_nat_gateway" "nat_gw" {
   subnet_id     = aws_subnet.public_subnets[0].id
 }
 
-
-
-
-/*creating public subnets for our VPC, with cidr block in our variables
-assigning CIDR block & assigining public IP  to instances in pub subnets 
+/* 
+create route tables ansd associate subnets to route table to recreate traffic flow as in previous asignments 
 */
-resource "aws_subnet" "public_subnets" {
-  count                   = length(var.public_subnets)
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = element(var.public_subnets, count.index)
-  availability_zone       = element(var.availability_zones, count.index)
-  map_public_ip_on_launch = true
+#Routing table for public subnets, with internet gateway
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
   tags = {
-    Name = "public-${element(var.availability_zones, count.index)}"
+    Name = "public_route_table"
   }
 }
 
-/*creating private subnets for our VPC, assigning cidr block &  AZ  via variables
-assigning CIDR block. 
-*/
-resource "aws_subnet" "private_subnets" {
-  count             = length(var.private_subnets)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = element(var.private_subnets, count.index)
-  availability_zone = element(var.availability_zones, count.index)
+#Associating the public subnets to the public routing table
+resource "aws_route_table_association" "public_route_table_assoc" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+#Routing table for private subnets, with nat gateway
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
 
   tags = {
-    Name = "private-${element(var.availability_zones, count.index)}"
+    Name = "private_route_table"
   }
 }
-  
-  /* security group allowing traffic to and from the VPc and the public subnets
+
+#Associating the private subnets to the private routing table
+resource "aws_route_table_association" "private_route_table_assoc" {
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+
+/* security group allowing traffic to and from instances in the public subnets
 limitting ssh to the ip defined in our designated variable.
 */
 resource "aws_security_group" "public_sg" {
@@ -154,11 +166,39 @@ resource "aws_security_group" "private_sg" {
   }
 
 }
+
+/*creating public subnets for our VPC, with cidr block in our variables
+assigning CIDR block & assigining public IP  to instances in pub subnets 
+*/
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(var.public_subnets, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-${element(var.availability_zones, count.index)}"
+  }
+ 
+}
+
+/*creating private subnets for our VPC, assigning cidr block &  AZ  via variables
+assigning CIDR block. 
+*/
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.private_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
+
+  tags = {
+    Name = "private-${element(var.availability_zones, count.index)}"
+  }
   
-/*
-Method to explicitly associate both security groups to subnets
-*/
+}
+  
+  
+  
+
     
-/* 
-Method to explicitly associate subnets to route table and route traffic flow as in previous asignments 
-*/
